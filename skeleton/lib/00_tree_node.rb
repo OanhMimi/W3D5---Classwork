3530d20b97ca9cee093e9c3c1feb9471064e3dd7
Ari Moshe <arimoshe@gmail.com>
2022-07-01 14:16:26 -0700

    Work on PolyTreeNode

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -9,21 +9,54 @@ class PolyTreeNode
     end
 
     def parent=(par) #p is an instance of another poly tree
-        if self.parent
-            @parent.children.inject([]) do |acc,ele|
-                if ele == self
-                    acc 
-                else
-                    acc << ele
+        unless par.nil?
+            if self.parent != nil
+                @parent.children.each do |ele|
+                    if ele == self
+                        @parent.children.delete(self)
+                    end
                 end
             end
-        end
 
-        return nil if nil
+        
         @parent = par 
         par.children << self
         self
+        else 
+            @parent = nil
+        end
+        
+    end
+
+    def add_child(child)
+        child.parent = self
+    end
+    
+    def remove_child(child)
+        if child.is_a?(PolyTreeNode) && self.children.include?(child)
+            child.parent = nil
+        else 
+            raise "not a child!!!!!!!!!!!"
+        end
     end
 
    
 end
+
+
+
+# n1 = PolyTreeNode.new("root1")
+# n2 = PolyTreeNode.new("root2")
+# n3 = PolyTreeNode.new("root3")
+
+# # connect n3 to n1
+# n3.parent = n1
+# # connect n3 to n2
+# n3.parent = n2
+
+# # this should work
+# raise "Bad parent=!" unless n3.parent == n2
+# raise "Bad parent=!" unless n2.children == [n3]
+
+# # this probably doesn't
+# raise "Bad parent=!" unless n1.children == []
